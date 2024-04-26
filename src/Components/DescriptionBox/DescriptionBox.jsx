14c0344bf239a1d51cbeffff6b10a8622044b6a9
Praveen Ramspra <you@example.com>
2024-04-26 22:20:57 +0530

    components from deepesh and kumaran

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (225)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Wrap yourself in comfort with our soft jacket, crafted from luxurious materials that caress your skin. Its plush texture provides warmth without weighing you down, making it perfect for cool evenings or brisk mornings. The tailored fit flatters your silhouette, while the gentle stretch ensures freedom of movement. With its versatile design, this jacket effortlessly transitions from casual outings to cozy nights in. Elevate your style and embrace relaxation with this essential wardrobe piece. Experience the embrace of softness with every wear.
+          </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
